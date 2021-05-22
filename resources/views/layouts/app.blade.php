bc362858502aba6a6e9003bed21f9a001d77a3b7
stefan.stojkov97@gmail.com <stefan.stojkov97@gmail.com>
2021-05-22 23:53:59 +0200

    Idea Model and Migrations

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        @livewireStyles
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
@@ -123,5 +124,6 @@
                 </div>
             </div>
         </main>
+        @livewireScripts
     </body>
 </html>
