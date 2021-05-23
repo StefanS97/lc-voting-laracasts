b56775927f6a107e4684ac2fc926a3b3317e91ec
stefan.stojkov97@gmail.com <stefan.stojkov97@gmail.com>
2021-05-23 20:35:32 +0200

    Add Category

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,18 +3,16 @@
 namespace Database\Factories;
 
 use App\Models\Category;
-use App\Models\Idea;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class IdeaFactory extends Factory
+class CategoryFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = Idea::class;
+    protected $model = Category::class;
 
     /**
      * Define the model's default state.
@@ -24,10 +22,7 @@ class IdeaFactory extends Factory
     public function definition()
     {
         return [
-            'user_id' => User::factory(),
-            'category_id' => $this->faker->numberBetween(1, 4),
-            'title' => ucwords($this->faker->words(4, true)),
-            'description' => $this->faker->paragraph(5),
+            'name' => $this->faker->words(2, true),
         ];
     }
 }
