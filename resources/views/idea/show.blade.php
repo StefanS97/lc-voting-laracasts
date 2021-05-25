62e24cd8b462497db0c544d444835e80fabff739
stefan.stojkov97@gmail.com <stefan.stojkov97@gmail.com>
2021-05-26 00:14:28 +0200

    Modal CSS

--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -8,6 +8,7 @@
         </a>
     </div>
     <livewire:idea-show :idea="$idea" :votesCount="$votesCount" />
+    <livewire:edit-idea />
     {{-- Comments container --}}
     <div class="comments-container relative space-y-6 md:ml-22 pt-4 my-8 mt-1">
         {{-- Comment container --}}
@@ -29,86 +30,21 @@
                             <div>10 hours ago</div>
                         </div>
                         <div x-data="{ isOpen: false }" class="flex items-center space-x-2">
-                            <button @click="isOpen = !isOpen" class="relative bg-gray-100 hover:bg-gray-200 border rounded-full h-7 transition duration-150 ease-in py-2 px-3">
-                                <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)"></svg>
+                            <div class="relative">
+                                <button @click="isOpen = !isOpen" class="relative bg-gray-100 hover:bg-gray-200 border rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                                    <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)"></svg>
+                                </button>
                                 <ul x-cloak x-show.transition.origin.top.left="isOpen" @click.away="isOpen = false" @keydown.escape.window="isOpen = false"
                                     class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 md:ml-8 top-8 md:top-6 right-0 md:left-0">
                                     <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark as Spam</a></li>
                                     <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete Post</a></li>
                                 </ul>
-                            </button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
         {{-- End Comment container --}}
-        {{-- Comment container --}}
-        <div class="comment-container relative bg-white rounded-xl flex mt-4">
-            <div class="flex flex-col md:flex-row flex-1 px-4 py-6">
-                <div class="flex-none">
-                    <a href="#">
-                        <img src="https://source.unsplash.com/200x200/?face&crop=face&v=2" alt="avatar" class="w-14 h-14 rounded-xl">
-                    </a>
-                </div>
-                <div class="w-full md:mx-4">
-                    <div class="text-gray-600 mt-3 line-clamp-3">
-                        Lorem ipsum dolor sit amet consectetur.
-                    </div>
-                    <div class="flex items-center justify-between mt-6">
-                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
-                            <div class="font-bold text-gray-900">John Doe</div>
-                            <div>&bull;</div>
-                            <div>10 hours ago</div>
-                        </div>
-                        <div x-data="{ isOpen: false }" class="flex items-center space-x-2">
-                            <button @click="isOpen = !isOpen" class="relative bg-gray-100 hover:bg-gray-200 border rounded-full h-7 transition duration-150 ease-in py-2 px-3">
-                                <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)"></svg>
-                                <ul x-cloak x-show.transition.origin.top.left="isOpen" @click.away="isOpen = false" @keydown.escape.window="isOpen = false"
-                                    class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 md:ml-8 top-8 md:top-6 right-0 md:left-0">
-                                    <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark as Spam</a></li>
-                                    <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete Post</a></li>
-                                </ul>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        {{-- End Comment container --}}
-        {{-- Comment container --}}
-        <div class="comment-container relative bg-white rounded-xl flex mt-4">
-            <div class="flex flex-col md:flex-row flex-1 px-4 py-6">
-                <div class="flex-none">
-                    <a href="#">
-                        <img src="https://source.unsplash.com/200x200/?face&crop=face&v=2" alt="avatar" class="w-14 h-14 rounded-xl">
-                    </a>
-                </div>
-                <div class="w-full md:mx-4">
-                    <div class="text-gray-600 mt-3 line-clamp-3">
-                        Lorem ipsum dolor sit amet consectetur.
-                    </div>
-                    <div class="flex items-center justify-between mt-6">
-                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
-                            <div class="font-bold text-gray-900">John Doe</div>
-                            <div>&bull;</div>
-                            <div>10 hours ago</div>
-                        </div>
-                        <div x-data="{ isOpen: false }" class="flex items-center space-x-2">
-                            <button @click="isOpen = !isOpen" class="relative bg-gray-100 hover:bg-gray-200 border rounded-full h-7 transition duration-150 ease-in py-2 px-3">
-                                <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)"></svg>
-                                <ul x-cloak x-show.transition.origin.top.left="isOpen" @click.away="isOpen = false" @keydown.escape.window="isOpen = false"
-                                    class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 md:ml-8 top-8 md:top-6 right-0 md:left-0">
-                                    <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark as Spam</a></li>
-                                    <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete Post</a></li>
-                                </ul>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        {{-- End Comment container --}}
-    </div>
     {{-- End Comments container --}}
 </x-app-layout>
