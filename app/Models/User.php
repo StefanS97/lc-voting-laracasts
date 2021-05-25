10079c2d52599cfe0d6507cf0e53b8d402db705f
stefan.stojkov97@gmail.com <stefan.stojkov97@gmail.com>
2021-05-25 13:13:06 +0200

    Idea Status

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,11 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Idea::class, 'votes');
     }
+
+    public function isAdmin()
+    {
+        return in_array($this->email, [
+            'stf@stf.com',
+        ]);
+    }
 }
